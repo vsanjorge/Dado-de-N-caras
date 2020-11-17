44214af959a5c1081bfde1e46f8b14749386bff9
Vsanjorge <vsanjorgeperez@gmail.com>
2020-11-17 18:10:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# Pequeño proyecto de prueba que lanza un dado de N caras, donde N es introducido por el usuario
+
+from random import choice
+import re
+
+dado = 0
+validacion = r"[0-9]+"
+
+while True:
+  dado = input("Número de caras del dado (0 para salir): ")
+  if re.match(validacion,dado):
+    dado = int(dado)
+    if dado == 0:
+      break
+    else:
+      print("El resultado de la tirada fue: " + str(choice(range(1,dado+1)))+"\n--------------------------")
+  else:
+    print("¡El valor introducido DEBE ser un número!\n--------------------------")
